671098c538241f8a5d33473719cc8f7d2b4ad750
meeranh <themeeranhassan@gmail.com>
2025-12-26 12:52:08 +0530

    feat: add main flake with zen-browser input

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "NixOS configuration for NeoBox";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, zen-browser, ... }@inputs: {
+    nixosConfigurations.NeoBox = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.users.neo = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
