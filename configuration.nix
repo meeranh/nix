173ebe299a17e79805592d359f5479239628a73f
meeranh <themeeranhassan@gmail.com>
2025-12-26 20:07:25 +0530

    fix: remove deprecated services.locate.localuser option

--- configuration.nix
+++ configuration.nix
@@ -131,7 +131,6 @@
   services.locate = {
     enable = true;
     package = pkgs.plocate;
-    localuser = null;
   };
 
   # ==========================================================================
